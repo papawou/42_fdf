3bfe0c58e7f509bec988ba34cc9e7410d718d724
Mendes Kevin <papawa.dev@gmail.com>
2022-02-04 13:15:20 +0100

    polop

--- inc/fdf.h
+++ inc/fdf.h
@@ -1,25 +1,28 @@
 #ifndef FDF_H
 #define FDF_H
 
-#include <libft/math.h>
+#include <libftmlx.h>
 #include <math.h>
 typedef struct s_scene
 {
-	t_mat4 cam;
+	t_mlx mlx;
+	t_mlx_win win;
+	t_mlx_img canvas;
+	
+	t_transform cam;
 } t_scene;
 
-
-//quat.c
+// quat.c
 t_quat quat_norm(t_quat q);
 t_quat quat_mult(t_quat a, t_quat b, int norm);
 t_mat4 quat_to_mat(t_quat q);
 t_quat quat_inv(t_quat q);
 
-//euler.c
+// euler.c
 t_mat4 euler_to_mat(t_euler v);
 t_quat euler_to_quat(t_euler v);
 
-//axig.c
+// axig.c
 t_quat axisg_to_quat(t_axisg ag);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,20 +1,6 @@
-#include <libftmlx/math.h>
 #include "fdf.h"
 
-/*
-t_mat4 create_orth_cam(float height, float width, float far, float near)
-{
-	t_mat4 m;
-	m = (t_mat4){MAT4_ID};
-	m.m[0][0] = 1 / width;
-	m.m[1][1] = 1 / height;
-	m.m[2][2] = -2 / (far - near);
-	m.m[2][3] = -(far + near) / (far - near);
-	return m;
-}
-*/
-
-t_mat4 create_orth_cam(float height, float width, float far, float near, t_transform cam)
+t_mat4 create_orth_cam(float width, float height, float far, float near, t_transform cam)
 {
 	t_mat4 mat_persp;
 	mat_persp = (t_mat4){MAT4_ID};
@@ -26,17 +12,43 @@ t_mat4 create_orth_cam(float height, float width, float far, float near, t_trans
 	return mult_mat_mat(mat_persp, transform_to_mat(transform_inv(cam)));
 }
 
+int render(t_scene *sc)
+{
+	// projection view
+	t_transform cam = {euler_to_quat((t_euler){0, 0, 0}),
+										 {-150, -100, 50}};
+	t_mat4 proj = create_orth_cam(300, 200, 100, 0, cam);
+
+	int map[2][3] = {{0, 1, 2}, {3, 4, 5}}; //[z][x]
+	t_transform tranf_map = {QUAT_ID, {0, 0, 0}};
+
+	int x = 0;
+	int z = 0;
+	while (z < 2)
+	{
+		x = 0;
+		while (x < 3)
+		{
+			t_vec4 out_point = mult_mat_vec(proj, (t_vec4){x, map[z][x], z, 1});
+			
+			++x;
+		}
+		++z;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
 
-	t_transform cam = {euler_to_quat((t_euler){0, 0, 0}),
-										 {-5, -5, 10}};
-	t_mat4 proj = create_orth_cam(10, 10, 10, 0, cam);
+	t_scene sc;
 
-	t_vec4 point = (t_vec4){0, 0, 0, 1};
-	t_vec4 out = mult_mat_vec(proj, point);
+	sc.mlx = mlx_init();
+	sc.win = mlx_new_window(sc.mlx, 300, 200, "fdf");
+	sc.canvas = ftmlx_new_img(sc.mlx, 300, 200);
 
+	mlx_loop_hook(sc.mlx, &render, &sc);
+	mlx_loop(sc.mlx);
 	return 0;
-} 
+}
