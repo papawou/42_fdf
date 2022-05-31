68161aa10a2a3e844b6d45ea3267c06d93de2cc0
Mendosita <papawa.dev@gmail.com>
2022-05-31 13:35:56 +0200

    start norm missing rast_line

--- src/controls.c
+++ src/controls.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   controls.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmendes <kmendes@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 04:31:18 by kmendes           #+#    #+#             */
+/*   Updated: 2022/05/31 04:32:10 by kmendes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-void rotate_camera(t_quat q, t_ftcam *cam)
+void	rotate_camera(t_quat q, t_ftcam *cam)
 {
 	cam->tranf = quat_mult_transform(q, cam->tranf);
 	ftmlx_update_cam(cam);
